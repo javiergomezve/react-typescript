68fb78d00cb5d25bd2742812431627b47e4e8656
Javier Gomez <javiergomezve@gmail.com>
2022-08-07 21:31:39 -0500

    [ADD] Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import './App.css';
-import { Sub } from './types';
+import { Sub, SubsResponseFromApi } from './types';
 import List from './components/List';
 import Form from './components/Form';
 
 interface AppState {
     subs: Sub[];
+    newSubsNumber: number;
 }
-
-const INITIAL_STATE = [
-    {
-        nick: 'dapelu',
-        subMonths: 3,
-        avatar: 'https://i.pravatar.cc/150?u=dapelu',
-        description: 'Moderator',
-    },
-    {
-        nick: 'javiergomezve',
-        subMonths: 12,
-        avatar: 'https://i.pravatar.cc/150?u=javiegomezve',
-    },
-];
 
 function App() {
     const [subs, setSubs] = useState<AppState['subs']>([]);
-
-    useEffect(() => {
-        setSubs(INITIAL_STATE);
-    });
+    const [newSubsNumber, setNewSubsNumber] =
+        useState<AppState['newSubsNumber']>(0);
 
     const handleNewSub = (newSub: Sub): void => {
         setSubs([...subs, newSub]);
+        setNewSubsNumber(n => n + 1);
     };
+
+    useEffect(() => {
+        const fetchSubs = (): Promise<SubsResponseFromApi> => {
+            return axios
+                .get('http://localhost:9001/subs')
+                .then(response => response.data);
+        };
+
+        const mapFromApiToSubs = (
+            apiResponse: SubsResponseFromApi
+        ): Array<Sub> => {
+            return apiResponse.map(subFromApi => ({
+                nick: subFromApi.nick,
+                subMonths: subFromApi.months,
+                avatar: subFromApi.profileUrl,
+                description: subFromApi.description,
+            }));
+        };
+
+        fetchSubs().then(mapFromApiToSubs).then(setSubs);
+    }, []);
 
     return (
         <div className="App">
